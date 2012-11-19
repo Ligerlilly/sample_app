5284fe75593392cf2fe0f77fe9e5e9aad395daba
Jason <quizathaderat@yahoo.com>
2012-11-19 13:19:15 -0800

    finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -20,4 +20,23 @@ module SessionsHelper
     self.current_user = nil
     cookies.delete(:remember_token)
   end
+  
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def current_user?(user)
+    user == current_user
+  end
+  #The redirect_back_or method redirects to the requested URI if it exists, 
+  #or some default URI otherwise, which we add to the Sessions controller create action 
+  #to redirect after successful signin
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+#The store_location method puts the requested URI in the session variable under the key :return_to
+  def store_location
+    session[:return_to] = request.url
+  end
 end
