33617e39bb74e4e356c76ce27b49af696faa07ed
Jason <quizathaderat@yahoo.com>
2012-12-22 13:04:38 -0800

    Profile page clean up tests passing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.paginate(page: params[:page])
-    @micropost  = current_user.microposts.build
   end
   
   def new
